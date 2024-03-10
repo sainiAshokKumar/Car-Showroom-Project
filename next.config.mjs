fc45637f42077d290ad26fadcc7a1e2dc96a1333
sainiAshokKumar <kumarsainia151@gmail.com>
2024-03-11 02:32:23 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+      images: {
+        // remotePatterns: [
+        //     {
+        //       protocol: 'https',
+        //       hostname: 'cdn.imagin.studio',
+        //       port: '3000',
+        //       pathname: '/getimage/**',
+        //     },
+        //   ],
+        domains: ['cdn.imagin.studio','imgd.aeplcdn.com','imagecdnsa.zigwheels.ae']
+      },
+};
 
 export default nextConfig;
